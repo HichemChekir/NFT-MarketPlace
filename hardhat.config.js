94c80617c93870c26d448429ef6d05360a7731cb
hichemChekir <hichem.chekir0@gmail.com>
2022-02-24 13:54:21 +0100

    some advancement + docker setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,20 +6,21 @@ const privateKey = fs.readFileSync(".secret").toString().trim() || "012345678901
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+module.exports = {/*
+  defaultNetwork: "matic",
   networks: {
     hardhat: {
       chainId: 1337,
 
     },
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+      url: "https://rinkeby.infura.io/v3/8ed7da6d4abe472a9668f831032ca04d",
       accounts: [privateKey]
     },
-
-
+*/
+  
     solidity: {
-      version: "0.8.0",
+      version: "0.8.4",
       settings: {
         optomizer: {
           enable: true,
@@ -27,5 +28,4 @@ module.exports = {
         }
       }
     }
-  },
-};
+  };
