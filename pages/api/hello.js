92ce367d3e91d3c00b183850d09b4fff5aa5fe5d
HichemChekir <hichem.chekir0@gmail.com>
2022-03-26 20:12:31 +0100

    adding API stuff

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+/*export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+*/
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+  res.send(Home())
+})
+
+let port = 4050
+app.listen(port,() => {
+  console.log("listening on port ${port}")
+} )
