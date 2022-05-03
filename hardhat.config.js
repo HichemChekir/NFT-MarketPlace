30e1f77a6eb40c20c9400b1ede2c9f9686db12f0
HichemChekir <hichem.chekir0@gmail.com>
2022-05-03 17:48:56 +0100

    new commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,12 +12,12 @@ module.exports = {/*
     hardhat: {
       chainId: 1337,
 
-    },
+    },*/
     mumbai: {
-      url: "https://rinkeby.infura.io/v3/8ed7da6d4abe472a9668f831032ca04d",
-      accounts: [privateKey]
+      url: "https://ropsten.infura.io/v3/8ed7da6d4abe472a9668f831032ca04d",
+      accounts: "1db2332a12a558f9380f232f8548abfc5f2b982a5cd0bb57b293843c8224344f"
     },
-*/
+
   
     solidity: {
       version: "0.8.3",
